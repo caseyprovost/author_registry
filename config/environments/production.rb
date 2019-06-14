56c1aea1bb295594186f46dd404a8b2bfcf4a97f
Casey Provost <casey.provost@spreemo.com>
2019-06-13 22:02:17 -0400

    fix env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { host: "author-registry.madrubyapp.com" }
+  #config.action_mailer.default_url_options = { host: "author-registry.madrubyapp.com" }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
