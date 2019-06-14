e23f7c7325f814eb4d2345613d89f44d5b507161
Casey Provost <casey.provost@spreemo.com>
2019-06-13 21:54:55 -0400

    generate uuid for authors in app

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,13 @@
 class Author < ApplicationRecord
+  after_initialize :set_uuid
+
+  def to_param
+    uuid
+  end
+
+  private
+
+    def set_uuid
+      self.uuid ||= SecureRandom.uuid
+    end
 end
